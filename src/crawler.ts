836f5003585057e4c9a207b4e3db13477ae0dccf
M. Gschwandtner <84477901+OnlyPain-ctrl@users.noreply.github.com>
2022-12-01 22:04:27 +0100

    moved operations from crawler to libs functions

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import { LinkChecker } from 'linkinator'
+import { dateHelper, fileParseHelper } from './libs/configHelpers'
 
 /* exports */
 
@@ -24,25 +25,14 @@ export async function runCrawler() {
 async function getConfig() {
     const fsRead = fs.readFileSync('./settings/crawl.conf', 'utf8')
 
-    const urls = fsRead
-        .split('\n')
-        .filter((line) => line.length > 0)
-        .filter((line) => !line.startsWith('#'))
-        .map((line) => line.trim())
+    const urls = fileParseHelper.splitNewLineRmComments(fsRead)
 
-    const concurrency = parseInt(
-        fsRead
-            .split('\n')
-            .filter((line) => line.includes('CONCURRENCY:'))[0]
-            .split(':')[1]
-            .trim()
+    const concurrency = fileParseHelper.getSettingValueAsInt(
+        fsRead,
+        'concurrency'
     )
 
-    const fullLog = fsRead
-        .split('\n')
-        .filter((line) => line.includes('FULL:'))[0]
-        .split(':')[1]
-        .trim()
+    const fullLog = fileParseHelper.getSettingValue(fsRead, 'full')
 
     if (!concurrency)
         throw new Error('Invalid value for concurrency (1, 50, 100)')
@@ -55,12 +45,8 @@ async function crawlerCheck(
     concurrency = 100,
     fullLog = false
 ) {
-    const date = new Date()
-
-    const day =
-        date.getDate() + '-' + date.getMonth() + '-' + date.getFullYear()
-    const time =
-        date.getHours() + '-' + date.getMinutes() + '-' + date.getSeconds()
+    const day = dateHelper.dayString()
+    const time = dateHelper.timeString()
 
     const folder = './logs/crawler/' + day + '/' + time + '_unfinished/'
 
@@ -72,26 +58,24 @@ async function crawlerCheck(
         linkChecker.on('link', (result) => {
             console.log(`${result.status} | ${result.url}`)
 
-            const reqPath = result.url.includes('base64')
-                ? 'base64(skipped)'
-                : result.url
+            const reqPath =
+                result.url.includes('base64') && result.url.includes('data:')
+                    ? 'base64(skipped)'
+                    : result.url
             const full = `${result.state}|${result.status}|${reqPath}|${result.parent}`
             const fullU8 = new Uint8Array(Buffer.from(full + '\n'))
 
-            const name = url
-                .replaceAll('/', '')
-                .replaceAll(':', '')
-                .replaceAll('https', '')
-                .replaceAll('http', '')
-                .replaceAll('www.', '')
-                .replaceAll('.', '-')
-
+            const name = fileParseHelper.urlToFilename(url)
             const first = result.status?.toString()[0]
+
             const statusPath = folder + '/' + name + '_[' + first + 'xx].csv'
             const path = folder + '/' + name + '_[full]' + '.csv'
 
             if (result.status != 200) {
-                fs.appendFileSync(folder + '/_all_[' + first + 'xx].csv', fullU8)
+                fs.appendFileSync(
+                    folder + '/_all_[' + first + 'xx].csv',
+                    fullU8
+                )
             }
 
             fs.appendFileSync(statusPath, fullU8)
